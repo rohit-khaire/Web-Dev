2a7193e1845dedeee9b5074f52b98c4bc742c6ab
rohit-khaire <athlius31@gmail.com>
2025-08-11 17:17:37 +0000

    Server created

---
+++ exp.js
@@ -0,0 +1,16 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const PORT = 3000;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
