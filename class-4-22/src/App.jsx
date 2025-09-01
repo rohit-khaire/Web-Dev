9f82775fc83ef5342614d4539d2eb5d4e9d51265
Rohit Khaire <athlius31@gmail.com>
2025-09-01 06:32:16 +0000

    created counter

--- class-4-22/src/App.jsx
+++ class-4-22/src/App.jsx
@@ -5,11 +5,11 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  function onClickHandler(){setCount(count+1)}
   return (
     <div>
       <h1>Rohit Khaire</h1>
-      <button>View Portfolio</button>
+      <button onClick={onClickHandler}>Counter: {count}</button>
     </div>
   )
 }
