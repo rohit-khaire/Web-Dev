7dd5ed16e5d61a646ad437b6be1dae08939e11a7
Rohit Khaire <athlius31@gmail.com>
2025-09-20 12:32:01 +0000

    Q2 Filtering String done

---
+++ class-6/Assignment/Q2/src/App.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState, useMemo } from "react";
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const words = ["rohit", "my", "name", "is", "khaire", "it's", "random", "word"];
+const TOTAL_LINES = 1000;
+const ALL_WORDS = [];
+for (let i = 0; i < TOTAL_LINES; i++) {
+  let sentence = "";
+  for (let j = 0; j < words.length; j++) {
+    sentence += words[Math.floor(words.length * Math.random())];
+    sentence += " ";
+  }
+  ALL_WORDS.push(sentence);
+}
+
+function App() {
+
+// In this assignment, you will create a component that renders a large list of sentences and includes an input field for filtering these items.
+// The goal is to use useMemo to optimize the filtering process, ensuring the list is only re-calculated when necessary (e.g., when the filter criteria changes).
+// You will learn something new here, specifically how you have to pass more than one value in the dependency array
+//x = "Rohit Khaire is great"
+//x.includes("is khaire") => false
+// x.includes("is grea") => true
+
+
+  const sentences = ALL_WORDS;
+  const [filter, setFilter] = useState("");
+
+  const filteredSentences = useMemo(()=>{
+    return sentences.filter(x => x.includes(filter))
+  },[filter])
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={(e) => {
+          setFilter(e.target.value);
+        }}
+      ></input>
+      {filteredSentences.map((word) => (
+        <div>{word}</div>
+      ))}
+    </div>
+  );
+}
+
+export default App
